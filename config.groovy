2bba68d7333e910aea2f00841f85b3a43a5f2521
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-05-31 19:46:28 -0600

    Update

--- config.groovy
+++ config.groovy
@@ -1,3 +1,4 @@
+import static org.codehaus.groovy.control.customizers.builder.CompilerCustomizationBuilder.*
 withConfig(configuration) {
     source(extension: 'sgroovy') {
         ast(groovy.transform.CompileStatic)
@@ -6,4 +7,7 @@ withConfig(configuration) {
     source(classValidator: { classNode -> !classNode.isInterface() } ) {
         ast(groovy.util.logging.Log4j2, visibilityId: 'Log4j2')
     }
+
+    System.setProperty("compileGroovy.project_dir", new File(new URI(getClass().protectionDomain.codeSource.location.path)).parent)
+    System.setProperty("compileGroovy.target_dir", configuration.getTargetDirectory().toString())
 }
--- src/main/java/io/github/sianabeeblebrox/boundless/meta/EntrypointASTTransform.java
+++ src/main/java/io/github/sianabeeblebrox/boundless/meta/EntrypointASTTransform.java
@@ -1,11 +1,15 @@
 package io.github.sianabeeblebrox.boundless.meta;
 
+import groovy.json.JsonParserType;
+import groovy.json.JsonSlurper;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.transform.ASTTransformation;
 import org.codehaus.groovy.transform.GroovyASTTransformation;
+
+import java.io.File;
 
 @GroovyASTTransformation(phase=CompilePhase.SEMANTIC_ANALYSIS)
 public class EntrypointASTTransform implements ASTTransformation {
@@ -14,7 +18,10 @@ public class EntrypointASTTransform implements ASTTransformation {
         ClassNode node = (ClassNode) nodes[1];
         //System.getProperties()
         //        .forEach((key, value) -> System.out.println(key + ": " + value));
-        System.out.println("XXX: " + node.getName() + " - " + System.getProperty("root_dir"));
+        System.out.println("XXX: " + node.getName() + " - " + System.getProperty("compileGroovy.project_dir")
+                + " - " + System.getProperty("compileGroovy.target_dir"));
+        //var modJSON = new JsonSlurper().setType(JsonParserType.LAX).parse(new File(System.getProperty("compileGroovy.project_dir") + "/src/main/resources/fabric.mod.json"));
+        //System.out.println(modJSON);
         //new File('C:\\Users\\exodu\\Desktop\\logs\\latest.log') << node.name;
     }
 }
